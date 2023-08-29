8bf6eaff7bdd2241279a9734911d7d3ec55e2a05
Ilma.Ameen <ilmaameen31@gmail.com>
2023-08-30 02:37:53 +0500

    code push

---
+++ src/Components/Question.js
@@ -0,0 +1,28 @@
+export const Data = [
+    {
+        question: "What is React?",
+        options: ["A programming language", "A JavaScript library for building user interfaces", "A server-side framework", "An operating system"],
+        answer: 1,
+      },
+      {
+        question: "Which hook in React is used for handling side effects?",
+        options: ["useEffect", "useState", "useSideEffect", "useLifecycle"],
+        answer: 0,
+      },
+      {
+        question: "In JavaScript, what is the purpose of the 'bind' method?",
+        options: ["To create a copy of an object", "To attach an event listener", "To add a property to an object", "To fix the value of 'this' within a function"],
+        answer: 3,
+      },
+      {
+        question: "What does JSX stand for?",
+        options: ["JavaScript XML", "JavaScript Xylophone", "JavaX System Extension", "Java Syntax Extension"],
+        answer: 0,
+      },
+      {
+        question: "Which feature was introduced in ECMAScript 6 (ES6) for declaring variables?",
+        options: ["var", "let", "const", "both 'let' and 'const'"],
+        answer: 3,
+      },
+      
+];
